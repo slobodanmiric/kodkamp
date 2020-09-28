989574b6740fb8119f3d051215408541d0d47ff4
slobodanmiric <slobamiric@gmail.com>
2020-09-28 21:23:02 +0200

    typed js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    
+    
+    var typed = new Typed('.element', {
+       strings: ['radimo frntend sajta', 'radimo custom WP', 'postavljamo sajt na server'],
+        typeSpeed: 100,
+        smartBackspace: true,
+        backSpeed: 100,
+        loop: true,
+        loopCount: Infinity,
+        startDelay: 1000
+    });
+    
+});
